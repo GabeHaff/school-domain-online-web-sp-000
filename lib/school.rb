b1a3d2382768b345fb260056f5ee32dc5886c9d4
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-08 06:51:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class=SCHOOL 
+school= school.new("Bayside High School")
+school.roster = {}
+roster["student"] = []
+roster["grade"] << "grade"
+roster 
+
+## school.roster do |student, grade| grade.each_to_i [1..12]
+##roster.add do roster << student, grade 
+
+def add_student(student,grade)
+end 
